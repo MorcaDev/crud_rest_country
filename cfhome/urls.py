9c7c201682893ba05e1115518777b49a7db83f8e
MorcaDesktop <morca225@gmail.com>
2024-05-09 20:44:20 -0500

    fix url endpoint

--- cfhome/urls.py
+++ cfhome/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('countries/', include('countries.urls'))
+    path('', include('countries.urls'))
 ]
